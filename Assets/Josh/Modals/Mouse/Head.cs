c0a94a68094eeb6de2ad8a28a7f47d341a2220c1
Joshua Frankle <joshuafrankle7@gmail.com>
2025-04-29 19:59:20 +0100

    Added Mouse Rig

---
+++ Assets/Josh/Modals/Mouse/Head.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class Head : MonoBehaviour
+{
+    [SerializeField] private Transform rootObject, followObject;
+    [SerializeField] private Vector3 positionOffset, rotationOffset, headBodyOffset;
+
+    private void LateUpdate()
+    {
+        rootObject.position = transform.position + headBodyOffset;
+        rootObject.forward = Vector3.ProjectOnPlane(followObject.up, Vector3.up).normalized;
+
+        transform.position = followObject.TransformPoint(positionOffset);
+        transform.rotation = followObject.rotation * Quaternion.Euler(rotationOffset);
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
